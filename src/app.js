21d1a8f84892be566cdf6b6669b1a5c42bdec3ef
millanda <97857799+millanda@users.noreply.github.com>
2023-02-01 16:57:46 -0800

    Hide broken image icon until new image is pulled

--- src/app.js
+++ src/app.js
@@ -45,6 +45,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.style.visibility = "visible";
 }
 
 function search(city) {
